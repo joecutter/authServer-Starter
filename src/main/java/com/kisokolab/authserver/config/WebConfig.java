237ce68b7aa4905cd43b1068577c581c881270fc
joecutter <cjnjenga@gmail.com>
2020-08-15 03:19:05 +0300

    cors

--- src/main/java/com/kisokolab/authserver/config/WebConfig.java
+++ src/main/java/com/kisokolab/authserver/config/WebConfig.java
@@ -23,7 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("*")
+                .allowedOrigins("*")
                 .allowedMethods("PUT", "DELETE", "GET", "POST");
     }
 }
